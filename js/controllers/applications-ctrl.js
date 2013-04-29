ac20f9c470baf5e53e6ef51f24f9074a5f3daba9
Stefan Wärting <stefan@warting.se>
2013-04-29 13:42:50 +0200

    packageName can contain numerics

--- js/controllers/applications-ctrl.js
+++ js/controllers/applications-ctrl.js
@@ -88,14 +88,66 @@ app.controller("ApplicationsCtrl", ["$scope", "$filter", "Mashape", "$cacheFacto
 	        if(warn) alert('must not contain ._');
 	        return false;
 	    }
+
+
+	    if(packageName.indexOf(".0") !== -1) {
+	        if(warn) alert('must not contain .0');
+	        return false;
+	    }
+	    if(packageName.indexOf(".1") !== -1) {
+	        if(warn) alert('must not contain .1');
+	        return false;
+	    }
+	    if(packageName.indexOf(".2") !== -1) {
+	        if(warn) alert('must not contain .2');
+	        return false;
+	    }
+	    if(packageName.indexOf(".3") !== -1) {
+	        if(warn) alert('must not contain .3');
+	        return false;
+	    }
+	    if(packageName.indexOf(".4") !== -1) {
+	        if(warn) alert('must not contain .4');
+	        return false;
+	    }
+	    if(packageName.indexOf(".5") !== -1) {
+	        if(warn) alert('must not contain .5');
+	        return false;
+	    }
+	    if(packageName.indexOf(".6") !== -1) {
+	        if(warn) alert('must not contain .6');
+	        return false;
+	    }
+	    if(packageName.indexOf(".7") !== -1) {
+	        if(warn) alert('must not contain .7');
+	        return false;
+	    }
+	    if(packageName.indexOf(".8") !== -1) {
+	        if(warn) alert('must not contain .8');
+	        return false;
+	    }
+	    if(packageName.indexOf(".9") !== -1) {
+	        if(warn) alert('must not contain .9');
+	        return false;
+	    }
 	    
-	    if(/^[a-z._]*$/.test(packageName) == false) {
-	        if(warn) alert('packagename contains invalid character, valid characters is: a-z . _');
+	    if(/^[0-9a-z._]*$/.test(packageName) == false) {
+	        if(warn) alert('packagename contains invalid character, valid characters is: 0-9a-z . _');
 	        return false;
 	    }
 	    
 	    var firstChar = packageName.charAt(0);
 	    var lastChar = packageName.charAt(packageName.length-1);
+
+	    if($scope.isNumber(firstChar)) {
+	        if(warn) alert('first character in packageName cannot be numeric');
+	    	return false;
+	    }
+	    if($scope.isNumber(lastChar)) {
+	        if(warn) alert('last character in packageName cannot be numeric');
+	    	return false;
+	    }
+
 	    if(firstChar == '.') {
 	        if(warn) alert('packageName cannot start with a . (dot)');
 	        return false;
@@ -115,5 +167,9 @@ app.controller("ApplicationsCtrl", ["$scope", "$filter", "Mashape", "$cacheFacto
 	    
 	    return true; // valid
 	};
+
+	$scope.isNumber = function(n) {
+	  return !isNaN(parseFloat(n)) && isFinite(n);
+	}
 
 }]);
